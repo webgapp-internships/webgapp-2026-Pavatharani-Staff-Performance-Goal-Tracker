6171938a3cfe3686ab56ad2d74b2f35ab2d06568
pavatharani06 <p34090564@gmail.com>
2026-05-19 08:44:41 +0530

    First commit

---
+++ hello.py
@@ -0,0 +1,30 @@
+print("hello")
+name= input("what is your name")
+print("hello," +name +"!")
+
+name= ("pavi")
+age= (19)
+height= (5.3)
+student= (True)
+
+print( name,age, height, student)
+
+a=15
+b=25
+c=a+b
+print(c)
+
+a=20
+b=8
+c=a-b
+print(c)
+
+a=12
+b=8
+c=a*b
+print(c)
+
+a=128
+b=4
+c=a/b
+print(c)
