dc8a7e4e5b50d10aa85a56b3e5f8d988d8ef81e3
pavatharani06 <p34090564@gmail.com>
2026-05-20 11:35:40 +0530

    one

--- operators.py
+++ operators.py
@@ -12,13 +12,13 @@ for sub in sub:
     marks1 = int(input("ENTER YOUR " + sub + " MARK : " ))
     total =total + marks1  
     if marks1 >= 90:
-        print("DISTINCTION")
+        print("O GRADE")
     elif marks1 >= 75:
-        print("FIRST CLASS")
+        print("A GRADE")
     elif marks1 >= 55:
-       print("SECOND CLASS")
+       print("B GRADE")
     elif marks1 >= 35:
-       print("THIRD CLASS")
+       print("C GRADE")
     else:
        print("SORRY : FAILED")
 
