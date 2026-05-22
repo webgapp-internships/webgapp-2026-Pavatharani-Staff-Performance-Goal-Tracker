1c3ffa0d999ab2e9056698260b1a81a1b95e5f4f
pavatharani06 <p34090564@gmail.com>
2026-05-22 10:50:37 +0530

    one

--- atm.py
+++ atm.py
@@ -6,15 +6,6 @@ class atm():
             print("correct PIN")
         else:
             print("wrong PIN")
-    def change_pin(self,old_pin,new_pin):
-        if self.__pin == old_pin:
-            self.__pin == new_pin
-            print("PIN changed successfully")
-        else:
-            print("Incorrect PIN")
 atm = atm(2327)
 user_pin = int(input("Enter your PIN :"))
 atm.check_pin(user_pin)
-old_pin = int(input("Enter old PIN :"))
-new_pin = int(input("Enter new PIN :"))
-atm.change_pin(old_pin,new_pin)
