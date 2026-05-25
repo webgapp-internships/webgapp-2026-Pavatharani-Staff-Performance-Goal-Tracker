02f71d1ef90de7dd130070e138378e27e5aedb3b
pavatharani06 <p34090564@gmail.com>
2026-05-26 00:39:04 +0530

    one

--- inherit.py
+++ inherit.py
@@ -1,11 +1,36 @@
-class department:
-    def __init__(self,name):
-        self.name =name
-    def (self,student):
-        return "..."
-class AIDS (department):
-    def student(self):
-        return f"{self.name}" ="pavi"
-class CSE (department):
-    def student(self):
-        return f"{self.name}" =""
+class project:
+
+    def __init__(self, student_name):
+        self.student_name = student_name
+
+    def details(self):
+        return "Project details"
+
+
+class project_name1(project):
+
+    def details(self):
+        return f"{self.student_name}: front end development"
+
+
+class project_name2(project):
+
+    def details(self):
+        return f"{self.student_name}: backend development"
+
+
+class project_name3(project):
+    def details(self):
+         return f"{self.student_name}: Full stack development "
+
+
+s1 = project_name1("sugasan")
+s2 = project_name2("pavi")
+s3 = project_name3("harini")
+
+
+print(s1.details())
+
+print(s2.details())
+
+print(s3.details())
